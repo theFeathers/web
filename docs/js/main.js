3fa3ec8d1a7920eeeb3b635e69a80708eba781ab
Muthu Kumar <muthukumar@thefeathers.in>
2020-01-29 06:43:18 +0530

    (feat) scrollSpy updates menu selection

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,15 +1,27 @@
 const q = document.querySelector.bind(document);
 
-const setActiveNav = (oldHash, newHash) =>
-	[
-		[oldHash, "remove"],
-		[newHash, "add"],
-	].forEach(([hash, act]) => q(`a[href*='#${hash}']`).classList[act]("active"));
+const debounce = (fn, ms = 0) => {
+	let timeoutId;
+	return function(...args) {
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => fn(...args), ms);
+	};
+};
 
-function hashHandler(e) {
-	const oldHash = e.oldURL.includes("#") ? e.oldURL.split("#")[1] : "home";
+const mainNavActive = "main-nav-active-item";
+
+const setActiveNav = hash => {
+	const oldActive = q("." + mainNavActive);
+	const newActive = q(`a[href*='#${hash}']`);
+	if (newActive) {
+		oldActive && oldActive.classList.remove(mainNavActive);
+		newActive.classList.add(mainNavActive);
+	}
+};
+
+function hashHandler() {
 	const newHash = location.hash ? location.hash.split("#")[1] : "home";
-	setActiveNav(oldHash, newHash);
+	setActiveNav(newHash);
 	closeOffscreenNav();
 }
 
@@ -69,38 +81,74 @@ const showService = id => {
 	);
 };
 
-const scrollSpy = () => {
-	// isInViewport(q("#aboutUs")) && alert();
+const isInViewport = el => {
+	const viewport = {
+		top: window.scrollY,
+		bottom: window.scrollY + window.innerHeight,
+		height: window.innerHeight,
+	};
+	const section = el.getBoundingClientRect();
+
+	// Note: we're only interested in the y-coordinate
+	// Greater numbers always indicate lower position in page
+	// For example, a is above b if b has a greater "y" than a
+
+	if (viewport.bottom < section.top) {
+		// (1) 100% of section is below viewport
+		return false;
+	}
+
+	if (viewport.top > section.bottom) {
+		// (2) 100% of section is above viewport
+		return false;
+	}
+
+	if (
+		// section top is above or at viewport top
+		section.top <= viewport.top && // and
+		// section bottom is below or at viewport bottom
+		section.bottom >= viewport.bottom
+	) {
+		// (3) 100% of viewport is section
+		return true;
+	}
+
+	let areaInView;
+
+	if (section.bottom > viewport.bottom) {
+		// (4) section is partly in viewport, bottom out
+		areaInView = viewport.bottom - section.top;
+	}
+
+	if (section.top < viewport.top) {
+		// (5) section is partly in viewport, top out
+		areaInView = section.bottom - viewport.top;
+	}
+
+	// (6.1) if true, section is over 50% in viewport
+	if (areaInView / viewport.height > 0.5) return true;
+	// (6.2) else, section is not enough in viewport yet
+	return false;
 };
 
-const isInViewport = section => {
-	const pageTopToViewportBottom = window.scrollY + window.innerHeight;
-	const rect = section.getBoundingClientRect();
+const scrollSpy = () => {
+	const scrolledTo = ["home", "about-us", "what-we-do", "contact"].find(x =>
+		isInViewport(q(`#${x}`)),
+	);
 
-	let bottomExcess = rect.bottom - pageTopToViewportBottom;
-	if (bottomExcess < 0) bottomExcess = 0;
-	let bottomExcessPercent = (bottomExcess / rect.height) * 100;
-
-	let topExcess = window.scrollY - rect.top;
-	if (topExcess < 0) topExcess = 0;
-	let topExcessPercent = (topExcess / rect.height) * 100;
-
-	if (topExcessPercent + bottomExcessPercent > 20) return false;
-	return true;
+	setActiveNav(scrolledTo);
 };
 
 window.addEventListener("load", () => {
-	location.hash && setActiveNav("home", location.hash.split("#")[1], true);
+	location.hash && setActiveNav(location.hash.split("#")[1]);
 
 	window.addEventListener("hashchange", hashHandler, false);
-
-	console.log(document.querySelectorAll("[id^='service']"));
 
 	document.querySelectorAll("[id^='service']").forEach(item => {
 		item.addEventListener("click", e => handleService(e.currentTarget.id));
 	}, false);
 
-	window.addEventListener("scroll", scrollSpy);
+	q("#app").addEventListener("scroll", debounce(scrollSpy, 100));
 
 	q("#nav-opener").addEventListener("click", openOffscreenNav);
 	q("#nav-closer").addEventListener("click", closeOffscreenNav);
